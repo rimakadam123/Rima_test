a8141d8c799636f4a235a849c1fb708f33ef8983
Rima <rima.a@media.net>
2017-12-06 19:59:30 +0530

    testing files updated

--- Rima_test_practise/test.js
+++ Rima_test_practise/test.js
@@ -1,4 +1,22 @@
 
-function myFunction() {
-    alert("Welcome! Rima!");
-}
+
+function check(form)
+ {
+
+ if(form.userid.value && form.email.value)
+  {
+    var content = document.getElementById("userid").value;
+        alert("Welcome" + " " + content);
+		clearInputs();
+
+   }
+   else
+   {
+    alert("Error Email or Username");
+    }
+   }
+   
+function clearInputs() {
+    document.getElementById("userid").value = "";
+    document.getElementById("email").value = "";
+    }
